88b000f02192298d806c5c9258879805b94fde4e
Paulo Rocha <paulorocha1988@gmail.com>
2020-04-19 09:14:45 +0100

    add customer file

---
+++ models/customers.rb
@@ -0,0 +1,20 @@
+require_relative("../db/sql_runner")
+
+class Customer
+
+    attr_reader :id
+    attr_accessor :name, :funds
+
+    def initialize(options)
+        @id = options["id"] if options["id"]
+        @name = options["name"]
+        @funds = options["funds"]
+    end
+
+    def save()
+        sql = "INSERT INTO customers (name, funds) VALUES ($1, $2) RETURNING id;"
+        values = [@name, @funds]
+        @id = SqlRunner.run(sql, values)[0]["id"].to_i
+    end
+
+end
